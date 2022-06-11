a11f32909ef34c5480e71e42b20c9eefce0fa5c3
Erem <rmrenex@gmail.com>
2022-06-11 12:56:36 +0200

    Create menu component

---
+++ components/menu.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+        backgroundColor: '#252525',
+        color: 'white',
+        height: 60,
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+    },
+    icon:{
+        width: 20,
+        height: 20,
+    }
+
+});
+
+const Menu = () => {
+  return (
+    <View style={styles.container}>
+        <Image style={styles.icon} source={require('../assets/pictures/home.png')}/>
+        <Image style={styles.icon} source={require('../assets/pictures/home.png')}/>
+        <Image style={styles.icon} source={require('../assets/pictures/home.png')}/>
+        <Image style={styles.icon} source={require('../assets/pictures/home.png')}/>
+    </View>
+  );
+}
+
+export default Menu;
