bc098274ba79e91ad9a05aeec3f38e565843c9a1
Erem <rmrenex@gmail.com>
2022-06-12 16:16:41 +0200

    Add navigation container

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,  { useCallback, useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './views/login';
+import Home from './views/home';
+import Register from './views/register';
+import Register2 from './views/register_2';
+import Charter from './views/charter';
+import OnBoarding from './views/onBoarding';
+import Success from './views/success';
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Register' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='Register2' component={Register2} />
+        <Stack.Screen name='Charter' component={Charter} />
+        <Stack.Screen name='Success' component={Success} />
+        <Stack.Screen name='OnBoarding' component={OnBoarding} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
